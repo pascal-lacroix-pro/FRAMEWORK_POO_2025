713729c8118996f5615974de1e616a61791b64e8
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-11-18 13:14:33 +0100

    Traduction des helpers en Helpers

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -3,7 +3,13 @@
     <?php foreach ($books as $book): ?>
         <li>
             <?php echo $book['title']; ?> :
-            <?php echo $book['resume']; ?>
+            <!-- On va tronquer le resume à 30 caractères -->
+            <!-- 
+                1. Tranformer notre Helpers en classe
+                2. Changer le PSR-4
+                3. composer dump-autoload
+            -->
+            <?php echo \Core\Helpers::truncate($book['resume'], 30); ?>
         </li>
     <?php endforeach; ?>
 </ul>
